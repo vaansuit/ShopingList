91c99971afda92d3b44535842862b4df22643e5c
vaansuit <vvansuit@outlook.com>
2023-04-15 10:45:20 -0300

    feat: created  configuration_page.dart

--- lib/src/configuration/configuration_page.dart
+++ lib/src/configuration/configuration_page.dart
@@ -14,7 +14,52 @@ class _ConfigurationPageState extends State<ConfigurationPage> {
       appBar: AppBar(
         title: const Text('Shop-List'),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Configurações',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Tema',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(height: 10),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.dark,
+              groupValue: ThemeMode.system,
+              title: const Text('Sistema'),
+              onChanged: (mode) {},
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.dark,
+              groupValue: ThemeMode.light,
+              title: const Text('Tema Claro'),
+              onChanged: (mode) {},
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.dark,
+              groupValue: ThemeMode.dark,
+              title: const Text('Tema Escuro'),
+              onChanged: (mode) {},
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Controle de dados',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(height: 10),
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text('Apagar cache e reiniciar app'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
