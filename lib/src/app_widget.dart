d6b0ed9a18647457e7fece1bd3fc834b8a6ff02c
vaansuit <vvansuit@outlook.com>
2023-04-15 11:26:57 -0300

    feat: now user can change themes, as default theme the app will take the user system preference

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,3 +1,4 @@
+import 'package:ShopList/src/stores/app_store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -9,12 +10,17 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Modular.setInitialRoute('/home/');
+    //escutando as mudanças de tema no aplicativo inteiro
+    final appStore = context.watch<AppStore>(
+      //Só ira alterar quando o themeMode for alterado
+      (store) => store.themeMode,
+    );
 
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
       darkTheme: darkTheme,
-      themeMode: ThemeMode.dark,
+      themeMode: appStore.themeMode.value,
       //Propriedades para fazer o flutter modular rodar
       routerDelegate: Modular.routerDelegate,
       routeInformationParser: Modular.routeInformationParser,
