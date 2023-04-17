2e1236e9346c7f1c50a8a8777c7f2dc23e0966c4
vaansuit <vvansuit@outlook.com>
2023-04-17 11:44:45 -0300

    feat: added methods to the class that implements configuration_service.dart

--- lib/src/configuration/service/configuration_service.dart
+++ lib/src/configuration/service/configuration_service.dart
@@ -1,7 +1,34 @@
 import 'package:ShopList/src/services/realm/models/configuration_model.dart';
+import 'package:realm/realm.dart';
 
 abstract class ConfigurationService {
   ConfigurationModel getConfigurationModel();
   void saveConfiguration(String themModeName, DateTime? date);
   void deleteAll();
+}
+
+//Implementando os contratos das configurações de tema
+class ConfigurationServiceImpl implements ConfigurationService {
+  final Realm realm;
+
+  ConfigurationServiceImpl(this.realm);
+
+  @override
+  ConfigurationModel getConfigurationModel() {
+    return realm.all<ConfigurationModel>().first;
+  }
+
+  @override
+  void saveConfiguration(String themModeName, DateTime? date) {
+    final model = getConfigurationModel();
+    realm.write(() {
+      model.themeModeName = themModeName;
+      model.syncDate = date;
+    });
+  }
+
+  @override
+  void deleteAll() {
+    realm.deleteAll();
+  }
 }
