3eda41e033ccc17653b326d9d52d6fdce3781fc0
vaansuit <vvansuit@outlook.com>
2023-04-17 15:05:58 -0300

    feat: created tables task and taskBoard

--- lib/src/services/realm/realm_config.dart
+++ lib/src/services/realm/realm_config.dart
@@ -1,9 +1,13 @@
 import 'package:ShopList/src/services/realm/models/configuration_model.dart';
 import 'package:realm/realm.dart';
 
+import 'models/task_model.dart';
+
 LocalConfiguration config = Configuration.local(
   [
     ConfigurationModel.schema,
+    Task.schema,
+    TaskBoard.schema,
   ],
   initialDataCallback: (realm) {
     realm.add(ConfigurationModel('system'));
